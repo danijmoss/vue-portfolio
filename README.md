21ec864ce2d3a2b18b5c0ce35ed41d76f3d264b2
Danielle Moss <dani.code.camp@gmail.com>
2019-01-21 20:55:58 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,29 +1,37 @@
-# my-portfolio
+# Vue Portfolio Project
+
+Portfolio project created using Vue CLI, Vue Router, and Sass. The project was completed for the Udemy course [Use Vuejs 2 to Create a Beautiful SEO-REady Website](https://www.udemy.com/vuejs-seo-website-course) by [Gary Simon](https://coursetro.com).
 
 ## Project setup
+
 ```
 yarn install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 yarn run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 yarn run build
 ```
 
 ### Run your tests
+
 ```
 yarn run test
 ```
 
 ### Lints and fixes files
+
 ```
 yarn run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home main-content">
+    <div class="left-content">
+      <h1>8 YEARS OF DESIGN</h1>
+
+      <p class="sutxt">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <p class="sutxt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, impedit.</p>
+    </div>
+
+    <div class="right-content img1">
+      <button>
+        <router-link to="/portfolio/one">Browse Work</router-link>
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     HelloWorld
   }
-}
+};
 </script>
+
+<style scoped>
+.img1 {
+  background: url("../assets/home.jpg");
+}
+</style>
