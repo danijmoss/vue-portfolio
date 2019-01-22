b8fcf793561f1b600308ff62816e2949f89287d4
Danielle Moss <dani.code.camp@gmail.com>
2019-01-22 16:26:07 -0500

    completed about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,40 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about main-content">
+    <div class="left-content">
+      <h1>ABOUT ME</h1>
+      <p class="subtxt">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit, laudantium.</p>
+
+      <button>
+        <router-link to="/portfolio/one">LOOK AT MY WORK</router-link>
+      </button>
+    </div>
+    <div class="right-content">
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore sequi in libero porro, enim tempora eius rem commodi eaque molestias corporis delectus pariatur dicta! Magnam soluta quibusdam amet. Quasi, nesciunt.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore sequi in libero porro, enim tempora eius rem commodi eaque molestias corporis delectus pariatur dicta! Magnam soluta quibusdam amet. Quasi, nesciunt.</p>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.left-content {
+  background-color: #ffe476;
+}
+
+.right-content {
+  background: #fff;
+  padding: 50px;
+  margin-top: 80px;
+  display: block;
+}
+
+button {
+  font-size: 1.5em;
+  margin-top: 100px;
+}
+
+.subtxt {
+  font-size: 1.5em;
+  margin-top: 30px;
+}
+</style>
+
